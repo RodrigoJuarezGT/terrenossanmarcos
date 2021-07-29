b2a0d1360af89147f3c6306806df210e49bc37b0
Rodrigo Juarez <rodrigo.arturojuarez@gmail.com>
2021-07-28 23:51:22 -0600

    se termina pagina de inicio

--- scripts/header/header.js
+++ scripts/header/header.js
@@ -2,9 +2,19 @@ var boton_menu = document.getElementById("icono_boton_menu")
 var seccion_navegacion = document.getElementById("seccion_navegacion")
 var flag_boton_menu = false;
 
+var hiden_icons = document.getElementsByClassName("hiden_icon");
+
+
+
 boton_menu.onclick = function(){
   
   if(flag_boton_menu){
+
+    for(let i=0; i< hiden_icons.length; i++){
+      hiden_icons[i].style.display = "none"
+      hiden_icons[i].classList.remove("class_anima_hiden_icon")
+    }
+
     seccion_navegacion.classList.remove("clase_animacion_header_desplegar")
     seccion_navegacion.classList.add("clase_animacion_header_ocultar")
     boton_menu.classList.add("fa-bars")
@@ -17,6 +27,13 @@ boton_menu.onclick = function(){
     boton_menu.classList.remove("fa-bars")
     boton_menu.classList.add("fa-times")
     flag_boton_menu = true
+
+    setTimeout(function(){
+      for(let i=0; i< hiden_icons.length; i++){
+        hiden_icons[i].classList.add("class_anima_hiden_icon")
+      }
+    }, 1000)
+
   }
   
 }
