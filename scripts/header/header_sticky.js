6f3ecccb5e7ad5da597818a4997d855ec7f62e8c
Rodrigo Juarez <rodrigo.arturojuarez@gmail.com>
2021-08-20 13:14:43 -0600

    se arregaln iconos y se agrega info inmueble

--- scripts/header/header_sticky.js
+++ scripts/header/header_sticky.js
@@ -8,12 +8,20 @@ function handleScroll()
   // Do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
   
-  if ((rootElement.scrollTop / scrollTotal ) > 0.23 ) //si el scroll va en el 6% de la altura
+  if ((rootElement.scrollTop / scrollTotal ) > 0.23 ) 
   {
+    if ((rootElement.scrollTop / scrollTotal ) > 0.95 ) 
+    {
+  
+      document.getElementById("sticky_header").classList.remove("mostrar_stycky_header");
+      document.getElementById("sticky_header").classList.add("esconder_stycky_header");
+    }else{
     // Mostrar el boton
 	  document.getElementById("sticky_header").classList.remove("esconder_stycky_header");
     document.getElementById("sticky_header").classList.add("mostrar_stycky_header");
     aux_header = false
+    }
+
   } else {
     // Ocultar boton
     if(aux_header == false){
@@ -21,6 +29,8 @@ function handleScroll()
       document.getElementById("sticky_header").classList.add("esconder_stycky_header");
     }
   }
+
+
 	
 }
 
