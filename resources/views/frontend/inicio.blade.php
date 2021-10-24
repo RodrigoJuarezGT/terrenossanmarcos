9801ba46c9dbeeb56c76126d9d59f79915e1f99e
Rodrigo <rodrigo.arturojuarez@gmail.com>
2021-10-24 01:18:21 -0600

    configuracion pagecontroller

---
+++ resources/views/frontend/inicio.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+soy incio
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{PageController};
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+
+Route::get('/', [App\Http\Controllers\PageController::class, 'inicio'])->name('inicio');
+
+
 
 
